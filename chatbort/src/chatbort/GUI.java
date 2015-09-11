ffe9d57b1944fc37ad3bf0833a5718fef517b5bc
stacksmash <kestelld@unm.edu>
2015-09-11 16:21:53 -0600

    added notation to GUI

--- chatbort/src/chatbort/GUI.java
+++ chatbort/src/chatbort/GUI.java
@@ -1,5 +1,18 @@
+/**
+ * @author Dan Kestell
+ * 
+ *         GUI is a class that runs the GUI which includes a load screen on 
+ *         which to enter your username, and a chat screen in shich you can
+ *         engage in a chat with Dan the CIA agent.
+ *         
+ *         This was based on a basic outline from 
+ *         http://codereview.stackexchange.com/questions/25461/simple-chat-room-swing-gui
+ *         
+ *         The GUI instantiates an instance of ChatBot.java and refers to it 
+ *         for replies to input.
+ */
+
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -7,7 +20,6 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.nio.charset.MalformedInputException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,22 +29,21 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-import javax.swing.text.DefaultHighlighter;
-import javax.swing.text.Highlighter;
-import javax.swing.text.Highlighter.HighlightPainter;
 
 public class GUI
 {
 
-  String name = "C.I.A. SecretChat v. 2.6.9";
-  JFrame init;
-  JFrame chatFrame = new JFrame(name);
-  JTextArea chatWindow;
-  JButton send;
-  JTextField newInput;
-  JTextField user;
-  ChatBot chatBot;
-
+  private String name = "C.I.A. SecretChat v. 2.6.9";
+  private JFrame init;
+  private JFrame chatFrame = new JFrame(name);
+  private JTextArea chatWindow;
+  private JButton send;
+  private JTextField newInput;
+  private JTextField user;
+  public ChatBot chatBot;
+  public String username;
+  
+  //runs the GUI, initiates the screen to the login phase 
   public static void main(String[] args)
   {
 
@@ -63,6 +74,7 @@ public class GUI
     });
   }
 
+  //Gui for login, user enters their username and then enters the chat interface
   public void login() throws IOException, ChatBot.MalformedInputException
   {
     chatFrame.setVisible(false);
@@ -89,9 +101,10 @@ public class GUI
     init.add(BorderLayout.SOUTH, enterServer);
     init.setSize(670, 500);
     init.setVisible(true);
-    chatBot = new ChatBot();
+    chatBot = new ChatBot(); //initializes the chatbot to interact with GUI
   }
 
+  //The actual chat interface, press send to enter chat to Dan the CIA operative
   public void startChat()
   {
     JPanel mainPanel = new JPanel();
@@ -119,7 +132,7 @@ public class GUI
     left.fill = GridBagConstraints.HORIZONTAL;
     left.weightx = 512.0D;
     left.weighty = 1.0D;
-
+   
     GridBagConstraints right = new GridBagConstraints();
     right.insets = new Insets(0, 10, 0, 0);
     right.anchor = GridBagConstraints.LINE_END;
@@ -127,6 +140,7 @@ public class GUI
     right.weightx = 1.0D;
     right.weighty = 1.0D;
 
+    //message box to enter message into
     southPanel.add(newInput, left);
     southPanel.add(send, right);
 
@@ -138,6 +152,8 @@ public class GUI
     chatFrame.setVisible(true);
   }
 
+  //Listener for chat window, listens for a button press in order to send 
+  //  message and recieve response.
   class SendListener implements ActionListener
   {
     public void actionPerformed(ActionEvent event)
@@ -161,9 +177,9 @@ public class GUI
       newInput.requestFocusInWindow();
     }
   }
-
-  String username;
-
+  
+  //Listens for start of chat, takes username from text box and saves it for
+  //  use in the GUI.
   class StartChatListener implements ActionListener
   {
     public void actionPerformed(ActionEvent event)
